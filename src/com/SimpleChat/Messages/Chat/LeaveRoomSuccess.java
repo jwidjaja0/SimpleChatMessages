13f94236e8a28e78d67d78130a6266d3ac6b619c
Joseph Widjaja <jwidjaja0@saddleback.edu>
2020-05-26 12:02:27 -0700

    update leaveroomrequest to include room name

--- src/com/SimpleChat/Messages/Chat/LeaveRoomSuccess.java
+++ src/com/SimpleChat/Messages/Chat/LeaveRoomSuccess.java
@@ -5,4 +5,13 @@ import com.SimpleChat.Messages.Interfaces.Chat;
 import java.io.Serializable;
 
 public class LeaveRoomSuccess implements Serializable, Chat {
+    private String roomName;
+
+    public LeaveRoomSuccess(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
 }
