3689873c349d4932430b14e91f724d24b1bc714d
Joseph Widjaja <jwidjaja0@saddleback.edu>
2020-05-26 12:06:18 -0700

    modified leaveroomrequest

--- src/com/SimpleChat/Messages/Chat/LeaveRoomRequest.java
+++ src/com/SimpleChat/Messages/Chat/LeaveRoomRequest.java
@@ -5,4 +5,13 @@ import com.SimpleChat.Messages.Interfaces.Chat;
 import java.io.Serializable;
 
 public class LeaveRoomRequest implements Serializable, Chat {
+    private String roomName;
+
+    public LeaveRoomRequest(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
 }
--- src/com/SimpleChat/Messages/Chat/LeaveRoomSuccess.java
+++ src/com/SimpleChat/Messages/Chat/LeaveRoomSuccess.java
@@ -5,13 +5,5 @@ import com.SimpleChat.Messages.Interfaces.Chat;
 import java.io.Serializable;
 
 public class LeaveRoomSuccess implements Serializable, Chat {
-    private String roomName;
 
-    public LeaveRoomSuccess(String roomName) {
-        this.roomName = roomName;
-    }
-
-    public String getRoomName() {
-        return roomName;
-    }
 }
