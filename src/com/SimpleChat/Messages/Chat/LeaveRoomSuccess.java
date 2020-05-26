feb46a417eb5b87106e372b29e3672c898e402da
Joseph Widjaja <jwidjaja0@saddleback.edu>
2020-05-26 11:43:34 -0700

    added LeaveRoom messages

---
+++ src/com/SimpleChat/Messages/Chat/LeaveRoomSuccess.java
@@ -0,0 +1,8 @@
+package com.SimpleChat.Messages.Chat;
+
+import com.SimpleChat.Messages.Interfaces.Chat;
+
+import java.io.Serializable;
+
+public class LeaveRoomSuccess implements Serializable, Chat {
+}
